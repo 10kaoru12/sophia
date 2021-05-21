8aef63688358e0b9c9ec3b3c97b1fe69e5caf69a
Kaoru Ishizawa <kaoru.ishizawa@nnlife.co.jp>
2021-05-21 10:58:25 +0000

    vscode remote developmentの導入

---
+++ src/sophia/app.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import prophet as Prophet
+
+print("Running https://facebook.github.io/prophet/docs/quick_start.html")
+
+df = pd.read_csv('example_wp_log_peyton_manning.csv')
+print(df.head())
+m = Prophet.Prophet()
+m.fit(df)
+future = m.make_future_dataframe(periods=365)
+future.tail()
+forecast = m.predict(future)
+print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
